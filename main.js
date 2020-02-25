ce0a7018c5c685be9642cfbcfbab6d2d35d90259
HONG YAN YU <SIRANPANG@HONGs-MBP-2.fios-router.home>
2020-02-25 15:53:53 -0500

    first

---
+++ main.js
@@ -0,0 +1,10 @@
+function changeHeading () {
+	document.getElementById('heading').style.backgroundColor="lightblue"
+}
+Now we can do a lot of fun stuff:
+
+function changeHeading () {
+	document.getElementById('heading').innerHTML="i clicked the button"
+}
+
+
